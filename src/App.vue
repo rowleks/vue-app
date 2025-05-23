54a4dd951375d7fe0037c5aed1d0140467d9dfa0
rowleks <momohrowland@gmail.com>
2025-05-24 00:43:08 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import HelloWorld from "./components/HelloWorld.vue";
+const name = "Rowland";
+</script>
+
+<template>
+  <div>
+    <!-- <HelloWorld msg="Vite + Vue" /> -->
+    <h1>Hello {{ name }}</h1>
+  </div>
+</template>
+
+<style scoped></style>
